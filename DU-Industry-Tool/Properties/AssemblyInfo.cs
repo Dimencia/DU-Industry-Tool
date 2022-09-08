0afb72527839cce92283fd9d7744e0cc982a59ae
tobitege <tobi1972@gmx.de>
2022-09-09 00:24:09 +0200

    v0.7.6 DLL version fix

--- DU-Industry-Tool/Properties/AssemblyInfo.cs
+++ DU-Industry-Tool/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DU-Industry-Tool")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Maintained by tobitege")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DU-Industry-Tool")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.7.0.0")]
-[assembly: AssemblyFileVersion("0.7.0.0")]
+[assembly: AssemblyVersion("0.7.6.0")]
+[assembly: AssemblyFileVersion("0.7.6.0")]
